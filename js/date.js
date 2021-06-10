f788dc7ebf85b92e4fa10a6de459aafcf2c7544d
rubaun <rubaun@gmail.com>
2021-06-10 00:03:33 -0300

    Correção do date

--- js/date.js
+++ js/date.js
@@ -1,5 +1,44 @@
-var data = new Date();
+var dataDate = new Date();
 
-//Exibe data de hoje
+/* Pode-se criar um objeto Date() de diversas formas.
+new Date() - data de hoje
+new Date(year, month, day, hours, minutes, seconds, milliseconds)
+new Date(milliseconds)
+new Date(date string)
+*/
+
+//Exibe a mensagem da promoção
 var mensagem = document.getElementById('mensagem');
-mensagem.textContent = hoje.getFullYear();
+mensagem.innerHTML = ofertaExpira(dataDate);
+
+//diadaSemana = new Date(dataDate.getTime() + 604800000);
+
+//document.getElementById('mensagem').textContent = diadaSemana.getDay();
+
+//O objeto date armazena o valor da data como milisegundos
+//86 400 000 milisegundos são 24hs
+
+function ofertaExpira(){
+    var diaDaSemana, dia, data, mes, ano, nomeDia, nomeMes;
+
+    diaDaSemana = new Date(dataDate.getTime() + 604800000);
+   
+    nomeDia = ['Domingo','Segunda','Terça','Quarta','Quinta','Sexta','Sábado'];
+
+    nomeMes = ['Janeiro','Fevereiro','Março','Abril','Maio','Junho','Julho',
+    'Agosto','Setembro','Outubro','Novembro','Dezembro'];
+
+    dia = nomeDia[diaDaSemana.getDay()];
+
+    data = diaDaSemana.getDate();
+
+    mes = nomeMes[diaDaSemana.getMonth()];
+
+    ano = diaDaSemana.getFullYear();
+
+    msgTermina = 'A nossa oferta termina ';
+    msgTermina += dia + ' <br>(' + data + ' de ' + mes + ' de ' + ano + ')';
+
+    return msgTermina;
+}
+
