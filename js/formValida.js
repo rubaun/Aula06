d0af1cb2de682f6048a8b1e59db993c87d39fbb7
rubaun <rubaun@gmail.com>
2021-06-15 23:06:03 -0300

    Iniciando formValida.js

--- js/formValida.js
+++ js/formValida.js
@@ -1,5 +1,47 @@
-var suiteEscolhida = document.forms[0].elements[0]
 
-suiteEscolhida.addEventListener('change', function(){
-    suiteEscolhida.
+//De acordo com a suite escolhida o hospede já tem marcados as refeições
+
+var suiteEscolhida = [document.forms[0].elements[0], document.forms[0].elements[1], document.forms[0].elements[2]]
+
+for (var i=[0]; i < suiteEscolhida.length; i++){
+
+suiteEscolhida[i].addEventListener('change', function(){
+    var almoco = document.forms[0].elements[11]
+    var janta = document.forms[0].elements[12]
+    almoco.checked = false;
+    janta.checked = false;
+
+    if(suiteEscolhida[0].checked){
+        almoco.checked = true;
+        janta.checked = true;
+    }
+
+    if(suiteEscolhida[1].checked){
+        almoco.checked = true;
+    }
+        
 })
+}
+
+//Tratando do CPF
+var cpf = document.getElementById('cpf');
+
+cpf.addEventListener('blur', function(){
+    var cpfValor = cpf.value;
+    var cpfCompleto = cpfValor.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/, 
+    function( regex, argumento1, argumento2, argumento3, argumento4 ) {
+           return argumento1 + '.' + argumento2 + '.' + argumento3 + '-' + argumento4;
+   })  
+   cpf.value = cpfCompleto; 
+   
+})
+
+cpf.addEventListener('input', function(){
+    if(cpf.validity.patternMismatch){
+        cpf.setcustomValidity('Por favor, digite apenas números, sem pontos e sem traço.')
+    } else{
+        cpf.setcustomValidity(' ');
+    }
+})
+
+
