4a41da207642dcc30b5877a56ba8bc17be5426dc
rubaun <rubaun@gmail.com>
2021-06-20 02:26:02 -0300

    enviando e-mail de contato com php

---
+++ escola/envio.php
@@ -0,0 +1,33 @@
+<?php
+
+if($_REQUEST["action"] == "sendmail")
+{
+    $mailContact = "Seu nome <contato@mundocriativo.art.br>";
+    $mailAuthor = $_POST["nome"];
+    $mailReply = $_POST["email"];
+    $mailCall = $_POST["tel"];
+    $mailSubject = "Contato via site";
+    $mailMessage = $_POST["mensagem"];
+
+    $headers = "Return-Path: ".$mailContact."\r\n";
+    $headers .= "From: ".$mailContact."\r\n";
+    $headers .= "Reply-To: ".$mailReply."\r\n";
+
+    $mailBody = "Nome: ".$mailAuthor."\r\n";
+    $mailBody .= "E-mail: ".$mailReply."\r\n";
+    $mailBody .= "Assunto: ".$mailSubject."\r\n";
+    $mailBody .= "Menssagem: ".$mailMessage."\r\n";
+
+    $sucess = mail($mailContact, $mailSubject, $mailBody, $headers)
+                or die("Problemas para enviar o e-mail");
+    
+    if($sucess)
+    {
+        echo "Email enviado com sucesso!";
+    }
+    else
+    {
+        echo "Ação inválida";
+    }
+}
+?>
